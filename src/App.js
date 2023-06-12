98e724b9645251664593d08a1c2b6d5789f886b1
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-06-12 21:37:41 +0530

    Game initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Game from "./Game";
 import "./App.css";
 
 function App() {
-  return <div className="App">Tic Tac Toe</div>;
+  return (
+    <div className="App">
+      <div>Tic Tac Toe</div>
+      <Game />
+    </div>
+  );
 }
 
 export default App;
